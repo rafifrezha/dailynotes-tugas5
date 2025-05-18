5acc0082717defa2fa75334204cc025ff2d7c551
rafifrezha <164522532+rafifrezha@users.noreply.github.com>
2025-05-18 14:23:38 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import NoteRoutes from './routes/NoteRoutes.js';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 app.use(cors({
     origin: 'https://fe-rafif-dot-f-10-pt-misbahudin.uc.r.appspot.com/',
     credentials: true
 }));
-app.use(cookieParser());
 app.use(express.json());
 app.use(NoteRoutes);
 
